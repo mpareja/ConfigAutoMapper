2d134a49f5bc63eb8e41aa0ecabb4ccc123476ae
Mario Pareja <pareja.mario@gmail.com>
2011-09-14 14:24:54 -0400

    Running unit tests after build.

--- build.bat
+++ build.bat
@@ -1,1 +1,8 @@
+@echo off
+set results=report
+
 msbuild.exe ConfigAutoMapper.sln -p:Configuration=Release
+
+if not exist %results% mkdir %results%
+lib\nunit\net-2.0\nunit-console.exe build\ConfigAutoMapper.Tests.dll /xml=%results%\ConfigAutoMapper.xml
+
