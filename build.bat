03031ce6807f3c88c3f925230816fbc6af4d2ccc
Mario Pareja <pareja.mario@gmail.com>
2011-09-14 15:12:50 -0400

    Build now performs rebuild.

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @echo off
 set results=report
 
-msbuild.exe ConfigAutoMapper.sln -p:Configuration=Release
+msbuild.exe ConfigAutoMapper.sln /t:Rebuild /p:Configuration=Release
 
 if not exist %results% mkdir %results%
 lib\nunit\net-2.0\nunit-console.exe build\ConfigAutoMapper.Tests.dll /xml=%results%\ConfigAutoMapper.xml
